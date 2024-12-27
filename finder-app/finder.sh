370e11450463a7f985c813a348fdd9d58cdbfb87
Rohan2039 <rohanskhadd@gmail.com>
2024-12-27 14:28:57 +0000

    added

---
+++ finder-app/finder.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+filesdir=No_dir
+searchstring=No_Str
+filesdir=$1
+searchstring=$2
+
+if [$filesdir ==No_dir] || [$searchstring ==No_Str]
+then
+  echo 1
+elif[-d filesdir]
+then
+  echo 1
+else
+  X=$(ls -c)
+  Y=$(grep -c searchstring *)
+  printMsg="The number of files are $X and the number of matching lines are $Y"
+  echo $printMsg
+fi
+  
+  
