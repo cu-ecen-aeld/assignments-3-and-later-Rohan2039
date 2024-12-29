fdae59f227abd82e6737438730dd61160c0202b2
Rohan S Khadd <74947465+Rohan2039@users.noreply.github.com>
2024-12-29 16:24:15 +0530

    Update writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+
+writefile=$1
+writestr=$2
+
+if [[ "$writefile" == "" || "$writestr" == ""]]
+then
+  echo "No dir and No Str"
+  exit 1
+else
+   echo "$writestr" > "$writefile"
+   if [ $? == 1 ]
+   then
+     echo "file could not creted"
+     exit 1
+    fi
+fi
 
